770eff07e15e88434c854e83ae10490d8d6f959b
albanjoseph <73558646+albanjoseph@users.noreply.github.com>
2020-12-06 23:19:07 +0000

    Updates from Overleaf

---
+++ report/realtime_iir_main.py
@@ -0,0 +1,48 @@
+from pyfirmata2 import Arduino
+import realtimescope as rts
+import scipy.signal as signal
+import IIR_filter as IIR
+import sys
+from pyqtgraph.Qt import QtGui #pyqtgraph plots different channels in seperate windows
+from timer import Timer
+
+t = Timer() #timer object
+
+app = QtGui.QApplication(sys.argv) #create a global QT application object
+running = True #signals to all threads in endless loops that we'd like to run these
+
+qtPanningPlot1 = rts.QtPanningPlot("Arduino 1st channel") #Two instances of plot windows
+qtPanningPlot2 = rts.QtPanningPlot("Arduino 2nd channel")
+
+samplingRate = 100 #sampling rate: 100Hz
+f1 = 1
+sos = signal.butter(5,2*f1/samplingRate, output = 'sos') #coeffs should filter everything except DC
+filt = IIR.IIRfilter(sos) #filter object instantiated
+
+# called for every new sample at channel 0 which has arrived from the Arduino
+# "data" contains the new sample
+def callBack(data):
+    #channel 0 data sent to the plotwindow
+    data = (data - 0.1)*400
+    qtPanningPlot1.addData(data)
+    ch1 = board.analog[1].read()
+    # 1st sample of 2nd channel might arrive later so need to check
+    if ch1:
+        # filtering channel 1 samples here:
+        ch1 = filt.filter(data) 
+        qtPanningPlot2.addData(ch1)
+
+
+PORT = Arduino.AUTODETECT #Get the Ardunio board.
+board = Arduino(PORT)
+
+board.samplingOn(1000 / samplingRate) # Set the sampling rate in the Arduino
+# Register the callback which adds the data to the animated plot
+board.analog[0].register_callback(callBack) #The function "callback" is called when data has arrived
+
+board.analog[0].enable_reporting() #Enable the callback
+board.analog[1].enable_reporting()
+
+app.exec_() #showing all the windows
+board.exit() #needs to be called to close the serial port
+print("finished")
---
+++ report/realtimescope.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+
+class QtPanningPlot:
+
+    def __init__(self,title):
+        self.win = pg.GraphicsLayoutWidget()
+        self.win.setWindowTitle(title)
+        self.plt = self.win.addPlot()
+        self.plt.setYRange(-1,7)
+        self.plt.setXRange(0,500)
+        self.curve = self.plt.plot()
+        self.data = []
+        # any additional initalisation code goes here (filters etc)
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(100)
+        self.layout = QtGui.QGridLayout()
+        self.win.setLayout(self.layout)
+        self.win.show()
+        
+    def update(self):
+        self.data=self.data[-500:]
+        if self.data:
+            self.curve.setData(np.hstack(self.data))
+
+    def addData(self,d):
+        self.data.append(d)
